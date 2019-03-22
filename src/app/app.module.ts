54116f26c48dc6e293d8962f076de445e3479d8a
Oleksii Aleksandrov <oleksii.aleksandrov@valor-software.com>
2019-03-22 18:27:00 +0200

    feat(layout): added layput components (WIP)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
-import { MatIcon, MatToolbar, MatToolbarRow } from '@angular/material';
+import { MatIconModule, MatSidenavModule, MatToolbar } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -15,14 +16,15 @@ import { MatIcon, MatToolbar, MatToolbarRow } from '@angular/material';
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
-    MatIcon,
-    MatToolbar,
-    MatToolbarRow
+    MatToolbar
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MatIconModule,
+    MatSidenavModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry
+      .addSvgIcon(
+        'ngx-meta-github',
+        this.domSanitizer.bypassSecurityTrustResourceUrl('../../assets/images/ico-github.svg')
+      ).addSvgIcon(
+      'ngx-meta-twitter',
+        this.domSanitizer.bypassSecurityTrustResourceUrl('../../assets/images/ico-twitter.svg')
+      );
   }
-
 }
