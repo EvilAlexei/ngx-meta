76466d6693915e96c2cd3cd4077a6b88265a98e7
Oleksii Aleksandrov <oleksii.aleksandrov@valor-software.com>
2019-03-26 20:34:58 +0200

    feat(layout): added content components

--- src/app/components/+getting-started/getting-started.component.ts
+++ src/app/components/+getting-started/getting-started.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-getting-started',
   template: `<markdown class="variable-binding" [data]="content"></markdown>`,
-  styleUrls: ['./getting-started.component.scss']
 })
 export class GettingStartedComponent {
   content: string = require('!!raw-loader!./getting-started.doc.md');
